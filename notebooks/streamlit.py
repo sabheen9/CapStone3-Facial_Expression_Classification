63be5c3c67ea00c1fe0cf0f697d6720b334d3c9b
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-27 12:47:26 -0700

    11th

--- notebooks/streamlit.py
+++ notebooks/streamlit.py
@@ -1,5 +1,19 @@
 import streamlit as st
-from Camera import VideoCamera
 
-VideoCamera()
+cam=st.selectbox(
+  'Choose a Cam', 
+  [
+    '',
+    'pitriverbridge/pitriverbridge.jpg',
+    'johnsongrade/johnsongrade.jpg',
+    'perez/perez.jpg',
+    'mthebron/mthebron.jpg',
+    'eurekaway/eurekaway.jpg',
+    'sr70us395/sr70us395.jpg',
+    'bogard/bogard.jpg',
+    'eastriverside/eastriverside.jpg',
+  ]
+)
+if cam:
+  st.image('https://cwwp2.dot.ca.gov/data/d2/cctv/image/' + cam)
 
