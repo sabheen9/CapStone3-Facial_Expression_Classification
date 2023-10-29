0eb957d66b77695eeb21c78eec72ab6ed9f96d98
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-28 22:17:52 -0700

    12

--- notebooks/streamlit.py
+++ notebooks/streamlit.py
@@ -15,6 +15,6 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 with st.sidebar:
-        st.header("POTATO RIGHT")
-        st.title("Potato Leaf Disease Early Prediction")
-        st.subheader("Early detection of diseases present in the leaf. This helps an user to easily detect the disease and identify it's cause.")
+        st.header("Facial Recognition App")
+        st.title("Facial Expression Recognition")
+        st.subheader("Facial expression recognition enables more natural and intuitive interactions between humans and computer systems, enhancing user experience and engagement.")
