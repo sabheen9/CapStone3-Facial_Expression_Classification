d8a95b871e70bf538b3cef0bc88a0ac71869984c
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-28 22:25:11 -0700

    12

--- notebooks/streamlit.py
+++ notebooks/streamlit.py
@@ -5,22 +5,6 @@ from PIL import Image, ImageOps
 import numpy as np
 import cv2
 
-st.set_page_config(
-    page_title="Potato Disease Classification"
-)
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
-with st.sidebar:
-        st.header("Facial Recognition App")
-        st.title("Facial Expression Recognition")
-        st.subheader("Facial expression recognition enables more natural and intuitive interactions between humans and computer systems, enhancing user experience and engagement.")
-
 
 
 img_file_buffer = st.camera_input("Take a picture")
