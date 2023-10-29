5edd1270ce5246ab185aabfad0be733366165f35
SABHEEN AMMAD <sabheen_ammad@hotmail.com>
2023-10-28 22:23:38 -0700

    12

--- notebooks/streamlit.py
+++ notebooks/streamlit.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import random
 from PIL import Image, ImageOps
 import numpy as np
+import cv2
+
 st.set_page_config(
     page_title="Potato Disease Classification"
 )
@@ -18,3 +20,20 @@ with st.sidebar:
         st.header("Facial Recognition App")
         st.title("Facial Expression Recognition")
         st.subheader("Facial expression recognition enables more natural and intuitive interactions between humans and computer systems, enhancing user experience and engagement.")
+
+
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(cv2_img))
+
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
